4d692bc71523f804d3d672f60e49e7131594b0ed
João Pedro <jotalmeida007@hotmail.com>
2021-02-18 22:53:22 -0300

    add get routes

--- web/lib/ex_mon_web/controllers/trainers_controller.ex
+++ web/lib/ex_mon_web/controllers/trainers_controller.ex
@@ -6,9 +6,8 @@ defmodule ExMonWeb.TrainersController do
   def create(conn, params) do
     params
     |> ExMon.create_trainer()
-    |> handle_response(conn)
+    |> handle_response(conn, "create.json", :created)
   end
-
 
   def delete(conn, %{"id" => id}) do
     id
@@ -16,7 +15,13 @@ defmodule ExMonWeb.TrainersController do
     |> handle_delete(conn)
   end
 
-    defp handle_delete({:ok, _trainer}, conn) do
+  def show(conn, %{"id" => id}) do
+    id
+    |> ExMon.fetch_trainer()
+    |> handle_response(conn, "show.json", :ok)
+  end
+
+  defp handle_delete({:ok, _trainer}, conn) do
       conn
       |> put_status(:no_content)
       |> text("")
@@ -24,10 +29,10 @@ defmodule ExMonWeb.TrainersController do
 
   defp handle_delete({:error, _reason} = error, _conn), do: error
 
-  defp handle_response({:ok, trainer}, conn) do
+  defp handle_response({:ok, trainer}, conn, view, status) do
     conn
-    |> put_status(:created)
-    |> render("create.json", trainer: trainer)
+    |> put_status(status)
+    |> render(view, trainer: trainer)
   end
-  defp handle_response({:error, _chageset} = error, _conn), do: error
+  defp handle_response({:error, _chageset} = error, _conn, _view, _status), do: error
 end
