028d8c12f87784c48cb6a053d7aa261146577360
João Pedro Alves <jotalmeida007@hotmail.com>
2021-02-21 00:34:25 -0300

    Create trainer-pokemon router

--- web/lib/ex_mon/trainer/pokemon.ex
+++ web/lib/ex_mon/trainer/pokemon.ex
@@ -28,6 +28,8 @@ defmodule ExMon.Trainer.Pokemon do
     %__MODULE__{}
     |> cast(params, @required)
     |> validate_required(@required)
+    # Challenge, verify if id_trainer exists
+    # |> assoc_constraint(:trainer)
     |> validate_length(:nickname, min: 2)
   end
 end
